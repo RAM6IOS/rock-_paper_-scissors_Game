6693e11d9290a4894bec814a97cf46de8958b6c7
RAM6IOS <58077232+RAM6IOS@users.noreply.github.com>
2021-08-04 22:41:32 +0100

    create interface

--- ock-paper-scissors project/ContentView.swift
+++ ock-paper-scissors project/ContentView.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var possiblemoves = ["Rock", "Paper", "Scissors"]
+    @State private var Colorbr :[Color] = [.red, .green, .yellow]
+    
+    
     @State private var correctMoves = Int.random(in: 0...2)
     @State private var scoreTitle = ""
     @State private var showingScore = false
@@ -21,44 +24,57 @@ struct ContentView: View {
     let agents = ["Cyril", "Lana", "Pam", "Sterling"]
     @State private var showGreeting = true
     var body: some View {
-        
+        NavigationView{
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [.white, .blue]), startPoint: .top, endPoint: .bottom)
+                .edgesIgnoringSafeArea(.all)
+      
         VStack {
-            Text(possiblemoves[correctMoves])
-          
             
+          
+        
             if scorePlyer >= 3 || scoreAi >= 3{
                
                 VStack{
+                    Spacer()
                 if scoreAi == 3{
+                   
                     Text("You loser try again")
-                        .font(.title3)
+                        .font(.system(size: 30, weight: .bold, design: .default))
                         .padding()
+                    Spacer()
                     Button("Play agen") {
                                 // flip the Boolean between true and false
                                 self.PlayAgen()
                             }.padding()
                              .foregroundColor(Color.white)
+                             .font(.system(size: 45, weight: .bold, design: .default))
                             .padding()
                              .background(Color.black)
                              .cornerRadius(25)
+                    Spacer()
                 }else{
-                    
+                    Spacer()
                     Text("You winer")
-                        .font(.title3)
+                        .font(.system(size: 30, weight: .bold, design: .default))
                         .padding()
+                    Spacer()
+                    
                     Button("Play agen") {
                                 // flip the Boolean between true and false
                                 self.PlayAgen()
                             }
                            .padding()
                            .foregroundColor(Color.white)
+                           .font(.system(size: 45, weight: .bold, design: .default))
                            .padding()
                            .background(Color.black)
                            .cornerRadius(25)
+                    Spacer()
                         
                     
                 }
-                
+                    Spacer()
                 }
                 
             }else{
@@ -69,14 +85,18 @@ struct ContentView: View {
                 ){
                     VStack{
                     Text(self.possiblemoves[number])
-                        .font(.title3)
-                        .foregroundColor(Color.white)
-                        .padding()
-                        .background(Color.black)
-                        .frame(width: 130, height: 130)
-                        .clipShape(Capsule())
+                            .font(.system(size: 45, weight: .bold, design: .default))
+                        
                         
                     }
+                    .padding()
+                    .foregroundColor(Color.white)
+                     .background(Colorbr[number])
+                     
+                     .cornerRadius(10)
+                     .padding()
+                     .padding()
+                   
                 }
                     
                         
@@ -84,12 +104,12 @@ struct ContentView: View {
                 
                 
             }.alert(isPresented: $showingScore) {
-                Alert(title: Text(scoreTitle), message: Text("Your score is ???"), dismissButton: .default(Text("Continue")) {
+                Alert(title: Text(scoreTitle), message: Text("AI choose: \(possiblemoves[correctMoves])"), dismissButton: .default(Text("Continue")) {
                     self.askQuestion()
                 })
             }
             }
-            
+            Spacer()
             HStack( spacing: 20){
                 VStack{
                 Text("ScorePlyer:")
@@ -105,7 +125,12 @@ struct ContentView: View {
                 }
                     
             }
+            Spacer()
                }
+        } .navigationBarTitle(Text("Rock-papr-Scissors").font(.system(size: 45, weight: .bold, design: .default)), displayMode: .inline)
+            
+        }
+       
     }
     func flagTapped(_ number: Int) {
         if number == 0 && correctMoves == 2{
